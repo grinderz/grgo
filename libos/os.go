83bdc48faa1c2f6ea2437c26704c092897d08481
Danilov Anton <frimen.c@gmail.com>
2023-05-15 06:47:44 +0300

    libos updated

--- libos/os.go
+++ libos/os.go
@@ -4,15 +4,28 @@ import (
 	"os"
 )
 
-func IsExists(path string) (bool, error) {
-	_, err := os.Stat(path)
+func IsExists(path string) (os.FileInfo, bool, error) {
+	stat, err := os.Stat(path)
 	if err == nil {
-		return true, nil
+		return stat, true, nil
 	}
 
 	if os.IsNotExist(err) {
-		return false, nil
+		return stat, false, nil
 	}
 
-	return false, err //nolint:wrapcheck
+	return stat, false, err //nolint:wrapcheck
+}
+
+func IsDirExists(path string) (bool, error) {
+	stat, exists, err := IsExists(path)
+	if err != nil || !exists {
+		return false, err
+	}
+
+	if stat.IsDir() {
+		return true, nil
+	}
+
+	return false, err
 }
